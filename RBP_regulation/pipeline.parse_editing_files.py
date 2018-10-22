b916e5af34f6e8f87e1bf550851012cf1cc315ea
Xiao Lab (UCLA) <43121068+gxiaolab@users.noreply.github.com>
2018-10-22 10:22:58 -0700

    Update pipeline.parse_editing_files.py

--- RBP_regulation/pipeline.parse_editing_files.py
+++ RBP_regulation/pipeline.parse_editing_files.py
@@ -21,12 +21,12 @@ with open(SiteFile) as SF:
 	next(SF)
 	for line in SF:
 		L = line.split()
-		SiteID, Mean_Coverage, Obs_Ratio, Batch_Ratio, exp_std, ABS, pval, FDR, gene, region, alu, batch = L
-		Mean_Coverage, Obs_Ratio, Batch_Ratio, pval, FDR = map(float, [Mean_Coverage, Obs_Ratio, Batch_Ratio, pval, FDR])
+		SiteID, Mean_Coverage, Obs_Ratio, Batch_Ratio, exp_std, ABS, pval, gene, region, alu, batch = L
+		Mean_Coverage, Obs_Ratio, Batch_Ratio, pval = map(float, [Mean_Coverage, Obs_Ratio, Batch_Ratio, pval])
 		chrom, pos, et = SiteID.split(':')
 		et = et[0] + et[5]
 		sig = "NA"
-		outline = [SiteID, sig, et, Obs_Ratio, Batch_Ratio, Mean_Coverage, 20, pval, FDR, gene, region, alu, batch]
+		outline = [SiteID, sig, et, Obs_Ratio, Batch_Ratio, Mean_Coverage, 20, pval, gene, region, alu, batch]
 		abs_dER = abs(Obs_Ratio - Batch_Ratio)
 		outlines[SiteID] = outline
 		if abs_dER > 0.05: 
